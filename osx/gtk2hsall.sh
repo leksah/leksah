8ec5a2bb01e44dcd8931709ab38376471c6d72d4
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-10-12 22:44:15 +1300

    Add ghc 7.4.2 to scripts

--- osx/gtk2hsall.sh
+++ osx/gtk2hsall.sh
@@ -3,6 +3,7 @@
 PATH=$HOME/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.0.3-i386/usr/bin:$PATH scripts/gtk2hs.sh
 PATH=$HOME/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.0.4-i386/usr/bin:$PATH scripts/gtk2hs.sh
 PATH=$HOME/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.4.1-i386/usr/bin:$PATH scripts/gtk2hs.sh
+PATH=$HOME/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.4.2-i386/usr/bin:$PATH scripts/gtk2hs.sh
 PATH=$HOME/Library/Haskell/bin:$HOME/ghc-7.6.1/bin:$PATH scripts/gtk2hs.sh
 
 
--- osx/makedmgs.sh
+++ osx/makedmgs.sh
@@ -3,6 +3,7 @@
 PATH=/Users/hamish/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.0.3-i386/usr/bin:$PATH osx/makedmg.sh
 PATH=/Users/hamish/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.0.4-i386/usr/bin:$PATH osx/makedmg.sh
 PATH=/Users/hamish/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.4.1-i386/usr/bin:$PATH osx/makedmg.sh
+PATH=/Users/hamish/Library/Haskell/bin:/Library/Frameworks/GHC.framework/Versions/7.4.2-i386/usr/bin:$PATH osx/makedmg.sh
 PATH=/Users/hamish/Library/Haskell/bin:$HOME/ghc-7.6.1/bin:$PATH osx/makedmg.sh
 
 
