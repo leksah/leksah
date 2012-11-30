7a7ab98068651c8deaf592261377b5cf37842328
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-11-30 16:34:55 +1300

    Build script changes

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
-rm -rf vendor/*/dist
-rm -rf vendor/gtk2hs/*/dist
+rm -rf vendor/*/dist \
+       vendor/gtk2hs/*/dist \
+       vendor/*/vendor/*/dist \
+       vendor/*/vendor/gtk2hs/*/dist
 
 cabal clean || exit
