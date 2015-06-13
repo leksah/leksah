2da74a2b3a7e35d04d5b77249b0ea582df13d903
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2015-06-13 14:24:03 +1200

    Update leksah-welcome package

--- data/leksah-welcome/src/Main.hs
+++ data/leksah-welcome/src/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP, TemplateHaskell #-}
 -- Welcome to Leksah.  This is a quick sample package for you to
 -- try things out with.  We hope it will be useful for those new
 -- to Haskell or just new to Leksah.
@@ -15,61 +14,37 @@
 
 -- When you are ready to create your own workspace and package.
 --   * Package -> New
---   * When asked for a root folder for your package select a new folder
---     with the desired name of your package
-
--- This is the "Main" module and it exports a "main" function
-module Main (
-    main
-) where
 
 -- Next we are importing some things from other modules.
 -- Leksah can normally addd these imports for you, just
 -- press Ctrl+R (OS X Command+R)
-import Control.Monad (unless)
 import Data.List (stripPrefix)
-import System.Exit (exitFailure)
-import Test.QuickCheck.All (quickCheckAll)
 
--- Simple function to create a hello message.
+-- | Simple function to create a hello message.
+--
+-- The following tells doctest & QuickCheck that if you strip "Hello "
+-- from the start of `hello s` you will be left with `s` (for any `s`).
+-- QuickCheck will create the test data needed to run this test.
+--
+-- To automatically run the tests when the package is built, click on
+-- the green tick on the tool leksah tool bar.
+--
+-- To see how this is configured take a look in leksah-welecome.cabal
+-- (you can open it with Package -> Edit).
+--
+-- The code that runs the tests is in the file test/Main.hs.
+--
+-- prop> stripPrefix "Hello " (hello s) == Just s
 hello s = "Hello " ++ s
 
--- QuickCheck is a great tool for writing tests.
--- The following tells QuickCheck that if you strip "Hello "
--- from the start of hello s you will be left with s (for any s).
--- QuickCheck will create the test data needed to run this test.
-prop_hello s = stripPrefix "Hello " (hello s) == Just s
-
--- exeMain : Executable Entry Point
--- --------------------------------
+-- | Executable Entry Point
+--
 -- Here is the entry point for the leksah-welcome executable
 --
 -- To run it
 --   * Select Leksah menu item Package -> Run (or the cogs on the toolbar)
 --   * Select "exeMain" and press Ctrl+Enter to run them in ghci
 --   * Run "leksah-wellcome" from the command line
-exeMain = do
+main = do
     putStrLn (hello "World")
-
--- testMain : Unit Testing Entry Point
--- -----------------------------------
--- This is the main function uses by the cabal test
---
--- To run these tests
---   * Select Leksah menu item Package -> Test
---   * Select the tick icon on the Leksa toolbar (to enable "cabal test" during builds)
---   * Select "testMain" and press Ctrl+Enter to run them in ghci
---   * Run "cabal test" from the command line in the package directory
-testMain = do
-    allPass <- $quickCheckAll -- Run QuickCheck on all prop_ functions
-    unless allPass exitFailure
-
--- This is a clunky, but portable, way to use the same Main module file
--- for both an application and for unit tests.
--- MAIN_FUNCTION is preprocessor macro set to exeMain or testMain.
--- That way we can use the same file for both an application and for tests.
-#ifndef MAIN_FUNCTION
-#define MAIN_FUNCTION exeMain
-#endif
-main = MAIN_FUNCTION
 
---
+++ data/leksah-welcome/test/Main.hs
@@ -0,0 +1,10 @@
+import Test.DocTest
+
+-- Unit Testing Entry Point
+-- ------------------------
+-- This is the main function uses by the cabal test
+--
+-- To run these tests
+--   * Select Leksah menu item Package -> Test
+--   * Select the tick icon on the Leksah toolbar (to enable "cabal test" during builds)
+main = doctest ["-isrc", "src/Main.hs"]
