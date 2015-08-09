1a707fb999f0d78293aebf71a6ffca0bbab2b7cf
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2015-08-10 01:07:28 +1200

    Fix bew leksah for new ghcjs-dom

--- bew/Main.hs
+++ bew/Main.hs
@@ -3,13 +3,11 @@ module Main (
 ) where
 
 import Graphics.UI.Gtk (postGUIAsync)
-import GHCJS.DOM.Document (documentGetBody)
 import GHCJS.DOM (runWebGUI, webViewGetDomDocument)
 
-main = do
+main =
   runWebGUI $ \ webView -> postGUIAsync $ do
     Just doc <- webViewGetDomDocument webView
-    Just body <- documentGetBody doc
     putStrLn "Bewleksah is a version of Leksah that can run in a web browser."
     putStrLn "Nothing working yet."
 
