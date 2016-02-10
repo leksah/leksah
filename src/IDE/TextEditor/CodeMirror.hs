c0a9d84bff13339965390a651f7412d0950c10cd
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-02-11 00:00:16 +1300

    Use new jsaddle

--- src/IDE/TextEditor/CodeMirror.hs
+++ src/IDE/TextEditor/CodeMirror.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveDataTypeable #-}
@@ -54,6 +55,8 @@ import GHC.Num (Num(..))
 import Data.Eq (Eq(..))
 import GHC.Float (Double)
 import qualified Data.Text as T (pack)
+import Control.Lens.Getter (to)
+import Control.Monad (liftM, (=<<))
 
 #ifdef LEKSAH_WITH_CODE_MIRROR
 import Control.Monad (unless)
@@ -62,9 +65,9 @@ import IDE.TextEditor.Class (TextEditor(..))
 import Graphics.UI.Gtk.WebKit.Types (WebView(..))
 import Control.Monad.Reader (ReaderT(..))
 import Language.Javascript.JSaddle
-       (valToObject, (#), JSContextRef, JSObjectRef, jsg, (<#), obj, js2,
-        js, JSM, js1, valToText, valToStr, js3, js0, MakeValueRef(..), MakeStringRef(..),
-        JSStringRef, JSValueRef, valToBool, strToText, valToNumber, MakeObjectRef)
+       (valToObject, (#), JSContextRef, Object, jsg, jsg2, (<#), obj, js2, jss,
+        js, JSM, js1, valToText, valToStr, js3, js0, ToJSVal(..), ToJSString(..),
+        JSString, JSVal, valToBool, strToText, valToNumber, MakeObject(..))
 import Control.Applicative ((<$>))
 import Control.Monad.Reader.Class (MonadReader(..))
 import Control.Concurrent (putMVar, newEmptyMVar, takeMVar, MVar, tryTakeMVar)
@@ -104,11 +107,11 @@ data CodeMirror = CodeMirror deriving( Typeable, Show )
 
 data CodeMirrorState = CodeMirrorState {
     cmContext        :: JSContextRef
-  , cmObject         :: JSObjectRef }
+  , cmObject         :: Object }
 type CM = ReaderT (WebView, CodeMirrorState) JSM
 webView :: CM WebView
 webView = fst <$> ask
-codeMirror :: CM JSObjectRef
+codeMirror :: CM Object
 codeMirror = cmObject . snd <$> ask
 runCM :: CodeMirrorRef -> CM a -> IDEM a
 runCM (v, mvar) f = liftIO $ do
@@ -138,59 +141,60 @@ runCM (v, mvar) f = liftIO $ do
 
 type CodeMirrorRef = (WebView, MVar CodeMirrorState)
 
-
-
+gCodeMirror x y   = jsg2 "CodeMirror" x y >>= valToObject
 body              = js  "body"
-value             = js  "value"
+setValue v        = jss "value" v
 setSize           = js2 "setSize"
-mode              = js  "mode"
-line              = js  "line"
-ch                = js  "ch"
-left              = js  "left"
-top               = js  "top"
-right             = js  "right"
-bottom            = js  "bottom"
-lastLine          = js0 "lastLine"
-getRange          = js2 "getRange"
-setValue          = js1 "setValue"
+setMode m         = jss "mode" m
+line              = js  "line" . to valToNumber
+setLine l         = jss "line" l
+ch                = js  "ch" . to valToNumber
+setCh c           = jss "ch" c
+setLeft l         = jss "left" l
+setTop t          = jss "top" t
+left              = js  "left" . to valToNumber
+top               = js  "top" . to valToNumber
+right             = js  "right" . to valToNumber
+bottom            = js  "bottom" . to valToNumber
+lastLine          = js0 "lastLine" . to valToNumber
+getRange x y      = js2 "getRange" x y . to valToStr
+callSetValue      = js1 "setValue"
 setBookmark'      = js2 "setBookmark"
-find              = js0 "find"
-from              = js  "from"
-getCursor :: (MakeValueRef a0, MakeObjectRef o) => a0 -> IndexPreservingGetter o (JSM JSValueRef)
-getCursor         = js1 "getCursor"
-isClean           = js0 "isClean"
+find              = js0 "find" . to valToObject
+from              = js  "from" . to valToObject
+getCursor x       = js1 "getCursor" x . to valToObject
+isClean           = js0 "isClean" . to valToBool
 markText          = js3 "markText"
-className         = js  "className"
+className         = "className"
 clearHistory      = js0 "clearHistory"
 callUndo          = js0 "undo"
-undo'             = js  "undo"
+undo'             = js  "undo" . to valToBool
 callRedo          = js0 "redo"
-redo'             = js  "redo"
+redo'             = js  "redo" . to valToBool
 historySize       = js0 "historySize"
 replaceRange      = js3 "replaceRange"
 insertAt          = js2 "replaceRange"
 replaceSelection  = js1 "replaceSelection"
-posFromIndex      = js1 "posFromIndex"
-lineCount         = js0 "lineCount"
-somethingSelected = js0 "somethingSelected"
+posFromIndex x    = js1 "posFromIndex" x . to valToObject
+lineCount         = js0 "lineCount" . to valToNumber
+somethingSelected = js0 "somethingSelected" . to valToBool
 setSelection      = js2 "setSelection"
 placeCursorAt     = js1 "setSelection"
 markClean         = js0 "markClean"
-coordsChar        = js2 "coordsChar"
+coordsChar pos n  = js2 "coordsChar" pos n . to valToObject
 charCoords        = js2 "charCoords"
 scrollIntoView    = js2 "scrollIntoView"
 getAllMarks       = js0 "getAllMarks"
-indexFromPos      = js1 "indexFromPos"
-getLineText :: (MakeValueRef a0, MakeObjectRef o) => a0 -> IndexPreservingGetter o (JSM JSValueRef)
-getLineText       = js1 "getLine"
-jsLength          = js  "length"
+indexFromPos p    = js1 "indexFromPos" p . to valToNumber
+getLineText l     = js1 "getLine" l
+jsLength          = js  "length" . to valToNumber
 
 cmIter :: CodeMirrorRef -> Int -> Int -> CM (EditorIter CodeMirror)
-cmIter cm l c = do
+cmIter cm l c =
     lift $ do
         i <- obj
-        i ^. line <# (fromIntegral l :: Double)
-        i ^. ch   <# (fromIntegral c :: Double)
+        i ^. setLine (fromIntegral l :: Double)
+        i ^. setCh   (fromIntegral c :: Double)
         return $ CMIter cm i
 
 newCMBuffer :: Maybe FilePath -> Text -> IDEM (EditorBuffer CodeMirror)
@@ -211,11 +215,10 @@ newCMBuffer mbFilename contents = do
 
             runjs $ do
                 document   <- jsg "document"
-                codeMirror <- jsg "CodeMirror"
                 code <- obj
-                code ^. value <# contents
-                code ^. mode <# "haskell"
-                cmObject <- codeMirror # (document ^. body, code) >>= valToObject
+                code ^. setValue contents
+                code ^. setMode "haskell"
+                cmObject <- gCodeMirror (document ^. body) code
 
                 cmObject ^. setSize "100%" "100%"
                 liftIO $ debugM "leksah" "newCMBuffer loaded"
@@ -237,8 +240,8 @@ newCMBuffer mbFilename contents = do
 instance TextEditor CodeMirror where
     data EditorBuffer CodeMirror = CMBuffer CodeMirrorRef
     data EditorView CodeMirror = CMView CodeMirrorRef
-    data EditorMark CodeMirror = CMMark JSObjectRef | CMCursor JSValueRef
-    data EditorIter CodeMirror = CMIter CodeMirrorRef JSObjectRef
+    data EditorMark CodeMirror = CMMark Object | CMCursor JSVal
+    data EditorIter CodeMirror = CMIter CodeMirrorRef Object
     data EditorTagTable CodeMirror = CMTagTable CodeMirrorRef
     data EditorTag CodeMirror = CMTag
 
@@ -247,17 +250,17 @@ instance TextEditor CodeMirror where
         m <- codeMirror
         lift $ do
             o <- obj
-            o ^. className <# name
+            (o <# className) name
             m ^. markText first last o
             return ()
     beginNotUndoableAction (CMBuffer cm) = return () -- TODO
     beginUserAction (CMBuffer cm) = return () -- TODO
     canRedo (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
-        lift $ (m ^. historySize ^. redo') >>= valToBool
+        lift $ m ^. historySize ^. redo'
     canUndo (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
-        lift $ (m ^. historySize ^. undo') >>= valToBool
+        lift $ m ^. historySize ^. undo'
     copyClipboard (CMBuffer cm) _ = return () -- TODO
     createMark (CMView cm) _refType (CMIter _ i) _tooltip = runCM cm $ do
         m <- codeMirror
@@ -284,46 +287,46 @@ instance TextEditor CodeMirror where
         lift $ do
             i <- obj
             l <- m ^. lastLine
-            i ^. line <# l
-            i ^. ch   <# m ^. getLineText l ^. jsLength
+            i ^. setLine l
+            i ^. setCh (m ^. getLineText l ^. jsLength)
             return $ CMIter cm i
-    getInsertMark (CMBuffer cm) = runCM cm . lift $ CMCursor <$> makeValueRef "head"
+    getInsertMark (CMBuffer cm) = runCM cm . lift $ CMCursor <$> toJSVal "head"
     getIterAtLine (CMBuffer cm) line = runCM cm $ cmIter cm line 0
-    getIterAtMark (CMBuffer cm) (CMMark mark) = runCM cm $ do
-        lift $ CMIter cm <$> (mark ^. find ^. from >>= valToObject)
+    getIterAtMark (CMBuffer cm) (CMMark mark) = runCM cm $
+        lift $ CMIter cm <$> mark ^. find ^. from
     getIterAtMark (CMBuffer cm) (CMCursor c) = runCM cm $ do
         m <- codeMirror
-        lift $ CMIter cm <$> ((m ^. getCursor c) >>= valToObject)
+        lift $ CMIter cm <$> m ^. getCursor c
     getIterAtOffset (CMBuffer cm) offset = runCM cm $ do
         m <- codeMirror
-        lift $ CMIter cm <$> ((m ^. posFromIndex (fromIntegral offset :: Double)) >>= valToObject)
+        lift $ CMIter cm <$> m ^. posFromIndex (fromIntegral offset :: Double)
     getLineCount (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
-        lift $ round <$> ((m ^. lineCount) >>= valToNumber)
+        lift $ round <$> m ^. lineCount
     getModified (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
-        lift $ not <$> ((m ^. isClean) >>= valToBool)
-    getSelectionBoundMark (CMBuffer cm) = runCM cm . lift $ CMCursor <$> makeValueRef "anchor"
+        lift $ not <$> m ^. isClean
+    getSelectionBoundMark (CMBuffer cm) = runCM cm . lift $ CMCursor <$> toJSVal "anchor"
     getSelectionBounds (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
         lift $ do
-            start <- (m ^. getCursor "start") >>= valToObject
-            end   <- (m ^. getCursor "end") >>= valToObject
+            start <- m ^. getCursor "start"
+            end   <- m ^. getCursor "end"
             return (CMIter cm start, CMIter cm end)
     getInsertIter (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
-        lift $ CMIter cm <$> (m ^. getCursor "head" >>= valToObject)
+        lift $ CMIter cm <$> m ^. getCursor "head"
     getSlice (CMBuffer cm) (CMIter _ first) (CMIter _ last) includeHidenChars = runCM cm $ do
         m <- codeMirror
-        lift $ m ^. getRange first last >>= valToText
+        lift $ m ^. getRange first last >>= strToText
     getStartIter (CMBuffer cm) = runCM cm $ cmIter cm 0 0
     getTagTable (CMBuffer cm) = return $ CMTagTable cm
     getText (CMBuffer cm) (CMIter _ first) (CMIter _ last) includeHidenChars = runCM cm $ do
         m <- codeMirror
-        lift $ m ^. getRange first last >>= valToText
+        lift $ m ^. getRange first last >>= strToText
     hasSelection (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
-        lift $ (m ^. somethingSelected) >>= valToBool
+        lift $ m ^. somethingSelected
     insert (CMBuffer cm) (CMIter _ p) text = runCM cm $ do
         m <- codeMirror
         lift $ m ^. insertAt text p >> return ()
@@ -351,7 +354,7 @@ instance TextEditor CodeMirror where
     setStyle (CMBuffer cm) _style = return () -- TODO
     setText (CMBuffer cm) text = runCM cm $ do
         m <- codeMirror
-        lift $ m ^. setValue text
+        lift $ m ^. callSetValue text
         return ()
     undo (CMBuffer cm) = runCM cm $ do
         m <- codeMirror
@@ -367,17 +370,17 @@ instance TextEditor CodeMirror where
         m <- codeMirror
         lift $ do
             pos <- obj
-            pos ^. left <# (fromIntegral x :: Double)
-            pos ^. top  <# (fromIntegral y :: Double)
-            CMIter cm <$> (m ^. coordsChar pos "window" >>= valToObject)
+            pos ^. setLeft (fromIntegral x :: Double)
+            pos ^. setTop (fromIntegral y :: Double)
+            CMIter cm <$> (m ^. coordsChar pos "window")
     getIterLocation (CMView cm) (CMIter _ i) = runCM cm $ do
         m <- codeMirror
         lift $ do
-            rect <- (m ^. charCoords i "window" >>= valToObject)
-            l <- rect ^. left   >>= valToNumber
-            r <- rect ^. right  >>= valToNumber
-            t <- rect ^. top    >>= valToNumber
-            b <- rect ^. bottom >>= valToNumber
+            rect <- m ^. charCoords i "window"
+            l <- rect ^. left
+            r <- rect ^. right
+            t <- rect ^. top
+            b <- rect ^. bottom
             return $ Rectangle (round l) (round t) (round $ r - l) (round $ b - t)
     getOverwrite (CMView cm) = return False -- TODO
     getScrolledWindow (CMView (v,_)) = liftIO . fmap (castToScrolledWindow . fromJust) $ widgetGetParent v
@@ -400,7 +403,7 @@ instance TextEditor CodeMirror where
     backwardCharC (CMIter cm i) = runCM cm $ do
         m <- codeMirror
         lift $ do
-            n <- m ^. indexFromPos i >>= valToNumber
+            n <- m ^. indexFromPos i
             i2 <- m ^. posFromIndex (n - 1)
             return (CMIter cm i2)
     backwardFindCharC (CMIter cm i) pred mbLimit = return Nothing -- TODO
@@ -409,20 +412,21 @@ instance TextEditor CodeMirror where
         m <- codeMirror
         lift $ do
             i2 <- obj
-            i2 ^. line <# i ^. line
-            i2 ^. ch <# (0 :: Double)
+            l <- i ^. line
+            i2 ^. setLine l
+            i2 ^. setCh (0 :: Double)
             return $ CMIter cm i2
     endsWord (CMIter cm i) = return False -- TODO
     forwardCharC (CMIter cm i) = runCM cm $ do
         m <- codeMirror
         lift $ do
-            n <- m ^. indexFromPos i >>= valToNumber
+            n <- m ^. indexFromPos i
             i2 <- m ^. posFromIndex (n + 1)
             return (CMIter cm i2)
     forwardCharsC (CMIter cm i) d = runCM cm $ do
         m <- codeMirror
         lift $ do
-            n <- m ^. indexFromPos i >>= valToNumber
+            n <- m ^. indexFromPos i
             i2 <- m ^. posFromIndex (n + fromIntegral d)
             return (CMIter cm i2)
     forwardFindCharC (CMIter cm i) pred mbLimit = return Nothing -- TODO
@@ -431,22 +435,22 @@ instance TextEditor CodeMirror where
         m <- codeMirror
         lift $ do
             i2 <- obj
-            l <- i ^. line >>= makeValueRef
-            i2 ^. line <# l
-            i2 ^. ch   <# m ^. getLineText l ^. jsLength
+            l <- i ^. line
+            i2 ^. setLine l
+            i2 ^. setCh (m ^. getLineText l ^. jsLength)
             return $ CMIter cm i2
     forwardWordEndC (CMIter cm i) = return Nothing -- TODO
     getChar (CMIter cm i) = return Nothing -- TODO
     getCharsInLine (CMIter cm i) = runCM cm $ do
         m <- codeMirror
-        lift $ round <$> (m ^. getLineText (i ^. line) ^. jsLength >>= valToNumber)
-    getLine (CMIter cm i) = runCM cm $ do
-        lift $ round <$> (i ^. line >>= valToNumber)
-    getLineOffset (CMIter cm i) = runCM cm $ do
-        lift $ round <$> (i ^. ch >>= valToNumber)
+        lift $ round <$> m ^. getLineText (i ^. line) ^. jsLength
+    getLine (CMIter cm i) = runCM cm $
+        lift $ round <$> i ^. line
+    getLineOffset (CMIter cm i) = runCM cm $
+        lift $ round <$> i ^. ch
     getOffset (CMIter cm i) = runCM cm $ do
         m <- codeMirror
-        lift $ round <$> (m ^. indexFromPos i >>= valToNumber)
+        lift $ round <$> m ^. indexFromPos i
     isStart i@(CMIter cm _) = do
         start <- getStartIter (CMBuffer cm)
         iterEqual start i
@@ -454,13 +458,13 @@ instance TextEditor CodeMirror where
         end <- getEndIter (CMBuffer cm)
         iterEqual end i
     iterEqual (CMIter cm i1) (CMIter _ i2) = runCM cm . lift $ do
-        l1 <- i1 ^. line >>= valToNumber
-        l2 <- i2 ^. line >>= valToNumber
+        l1 <- i1 ^. line
+        l2 <- i2 ^. line
         if l1 /= l2
             then return False
             else do
-                c1 <- i1 ^. ch >>= valToNumber
-                c2 <- i2 ^. ch >>= valToNumber
+                c1 <- i1 ^. ch
+                c2 <- i2 ^. ch
                 return $ c1 == c2
     startsLine i = (== 0) <$> getLineOffset i
     startsWord (CMIter cm i) = return False -- TODO
@@ -469,15 +473,16 @@ instance TextEditor CodeMirror where
         m <- codeMirror
         lift $ do
             i2 <- obj
-            i2 ^. line <# (fromIntegral l :: Double)
-            i2 ^. ch <# (0 :: Double)
+            i2 ^. setLine (fromIntegral l :: Double)
+            i2 ^. setCh (0 :: Double)
             return $ CMIter cm i2
     atLineOffset (CMIter cm i) column = runCM cm $ do
         m <- codeMirror
         lift $ do
             i2 <- obj
-            i2 ^. line <# i ^. line
-            i2 ^. ch <# (fromIntegral column :: Double)
+            l <- i ^. line
+            i2 ^. setLine l
+            i2 ^. setCh (fromIntegral column :: Double)
             return $ CMIter cm i2
     atOffset (CMIter cm _ ) offset = getIterAtOffset (CMBuffer cm) offset
     atStart (CMIter cm _) = getStartIter (CMBuffer cm)
@@ -517,7 +522,7 @@ instance TextEditor CodeMirror where
         liftIO $ do
             v `widgetAddEvents` [ButtonReleaseMask]
             id1 <- (`reflectIDE` ideR) $ v `onIDE` buttonReleaseEvent $ do
-                mod <- lift $ eventModifier
+                mod <- lift eventModifier
                 case mod of
                     [Control] -> f >> return True
                     _             -> return False
