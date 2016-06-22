ccf8c59783b4f39f55db200bed8cc62dc2cddca2
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-06-22 21:09:16 +1200

    Split travis (timed out jobs do not update cache)

--- travis/build.sh
+++ travis/build.sh
@@ -1,24 +1,16 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 echo $PATH
 export LC_ALL=C.UTF-8
 
-if [ -d .cabal ]; then cp -a .cabal /root; fi
-if [ -d .ghc ]; then cp -a .ghc /root; fi
-
-cabal --version
-ghc --version
-cabal update
-cabal new-build ghcjs-dom || cabal new-build ghcjs-dom
-
-# update the cache in case we time out later
-rm -rf .cabal
-cp -a /root/.cabal ./
-rm -rf .ghc
-cp -a /root/.ghc ./
-
-cabal new-build || cabal new-build
-cabal sdist
+if [[ -d .cabal && -d .ghc ]]; then
+    cp -a .cabal .ghc /root
+    cabal update
+    cabal new-build
+else
+    cabal update
+    cabal new-build ghcjs-dom
+fi
 
 # update the cache
 rm -rf .cabal
