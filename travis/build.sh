f4ef6ef70ef284d9bd80b57322e9cf794984b6b1
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-06-23 13:05:48 +1200

    Split travis (timed out jobs do not update cache)

--- travis/build.sh
+++ travis/build.sh
@@ -9,10 +9,14 @@ fi
 
 cabal update
 
+# Detect if the cache is warmed up.  If so build both versions.
 if ghc-pkg --package-db /root/.cabal/store/ghc-`ghc --numeric-version`/package.db list | grep ghcjs-dom; then
     cabal new-build
+    all_done=true
 else
+    # Just build ghcjs-dom
     cabal new-build ghcjs-dom
+    all_done=false
 fi
 
 # update the cache
@@ -20,3 +24,8 @@ rm -rf .cabal
 cp -a /root/.cabal ./
 rm -rf .ghc
 cp -a /root/.ghc ./
+
+if [ "$all_done" = false ]; then
+    echo "Still warming up the Cache.  Please rerun this build."
+    exit 1
+fi
