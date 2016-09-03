001a96533eb3131385b8f730e36c367c754406ae
Martijn Fleuren <martijnfleuren@gmail.com>
2016-09-03 14:06:27 +0200

    LPaste now uses post, whitespace is properly encoded

--- src/IDE/LPaste.hs
+++ src/IDE/LPaste.hs
@@ -1,52 +1,24 @@
 module IDE.LPaste where
 
 import Data.Maybe
-import Data.String.Utils
 import Network.HTTP
-import Network.Stream
 
--- | Post record type containing the @content@ and optionally the @title@.
-data Post = Post {
-    title :: Maybe String,
-    content :: String
-}
-
--- | Parameters of the LPaste upload. Maybe fields are optional
-data Parameters = Parameters {
-    private :: Bool,
-    author  :: Maybe String,
-    channel :: Maybe String,
-    email   :: Maybe String
-}
-
--- | Make the link from a parameter and a post record
-makeLink :: Parameters -> Post -> String
-makeLink parameters post =
-    replace " " "+" $
-    concat [ "http://lpaste.net/new"
-           , "?private="
-           , if private parameters then "Private" else "Public"
-           , "&title="
-           , fromMaybe "" $ title post
-           , "&author="
-           , fromMaybe "" $ author parameters
-           , "&language=haskell"
-           , "&channel="
-           , fromMaybe "" $ channel parameters
-           , "&paste="
-           , content post
-           , "&email="
-           , fromMaybe "" $ email parameters
-           ]
+type Parameter = (String, String)
 
 -- | Default Leksah parameters
-defaultParameters :: Parameters
-defaultParameters = Parameters {
-    private = False,
-    author = Just "Leksah Haskell IDE",
-    channel = Nothing,
-    email = Nothing
-}
+leksahParams :: [Parameter]
+leksahParams =
+    [ ("public", "Public")
+    , ("author" , "Leksah Haskell IDE")
+    , ("channel", "")
+    , ("language", "haskell") -- Might change in the future
+    , ("email"  , "")
+    ]
+
+mkReq :: String -> String
+mkReq str =
+    let post = [("title", "leksah test"), ("paste", str)] -- Randomly generate te title
+    in "http://lpaste.net/new?" ++ urlEncodeVars (leksahParams ++ post)
 
 -- | Lookup the value of the location header.
 locationLookup :: [Header] -> String
@@ -54,17 +26,13 @@ locationLookup [] = ""
 locationLookup (Header k v:xs) =
     if HdrLocation == k then v else locationLookup xs
 
--- | Dirty function, has to be removed or made better at leaast.. .:'(
-extractHeaders :: Either ConnError (Response a) -> [Header]
-extractHeaders result =
-    case result of
-        Left _ -> []
-        Right rsp -> rspHeaders rsp
-
 -- | Main purpose function: Perform all the necessary actions for uploading and
 -- return the link to the submission.
+--
+-- The simpleHTTP will return a Right on succes, the unwrapping is a workaround
+-- and should be replaced with an Except monad.
 uploadSelected :: String -> IO String
 uploadSelected str =
-    let link = makeLink defaultParameters (Post { title = Nothing, content = str})
-    in ((++) "http://lpaste.net" . locationLookup . extractHeaders) <$> simpleHTTP (getRequest link)
+    (\(Right x) -> (++) "http://lpaste.net" . locationLookup $ rspHeaders x) <$>
+    simpleHTTP (postRequest $ mkReq str)
 
