f37020cd35e169cfcc02e10bd390d82bb922d450
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-09-28 23:15:04 +1300

    Use -j4 in leksah.sh

--- leksah.sh
+++ leksah.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cabal new-build exe:leksah-server exe:leksah exe:leksahecho || exit
+cabal new-build -j4 exe:leksah-server exe:leksah exe:leksahecho || exit
 PATH=`pwd`/dist-newstyle/build/leksah-server-0.16.1.0/build/leksah-server:`pwd`/dist-newstyle/build/leksah-server-0.16.1.0/build/leksahecho:$PATH leksah_datadir=`pwd` dist-newstyle/build/leksah-0.16.1.0/build/leksah/leksah $@
 
