d5c35419ce763d54ab1da489d2dd2ef7744536c5
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-12-02 14:22:00 +1300

    Bump version to 0.16.2.0

--- leksah.sh
+++ leksah.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cabal new-build -j4 exe:leksah-server exe:leksah exe:leksahecho || exit
-PATH=`pwd`/dist-newstyle/build/leksah-server-0.16.1.0/build/leksah-server:`pwd`/dist-newstyle/build/leksah-server-0.16.1.0/build/leksahecho:$PATH leksah_datadir=`pwd` dist-newstyle/build/leksah-0.16.1.0/build/leksah/leksah $@
+PATH=`pwd`/dist-newstyle/build/leksah-server-0.16.2.0/build/leksah-server:`pwd`/dist-newstyle/build/leksah-server-0.16.2.0/build/leksahecho:$PATH leksah_datadir=`pwd` ./dist-newstyle/build/leksah-0.16.2.0/build/leksah/leksah $@
 
