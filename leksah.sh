a91ad63071813466adc8a4d45bb368492e540166
Anton-Latukha <anton.latukha@gmail.com>
2017-02-15 05:09:20 +0200

    Modified ./leksah.sh to meaningful form

--- leksah.sh
+++ leksah.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+VERSION='0.16.2.2'
+LEKSAH_SERVER_BUILD_DIR=`pwd`'/dist-newstyle/build/leksah-server-'"$VERSION"'/build'
+LEKSAH_BUILD_DIR='dist-newstyle/build/leksah-'"$VERSION"'/build'
+
 cabal new-build -j4 exe:leksah-server exe:leksah exe:leksahecho || exit
-PATH=`pwd`/dist-newstyle/build/leksah-server-0.16.2.0/build/leksah-server:`pwd`/dist-newstyle/build/leksah-server-0.16.2.0/build/leksahecho:$PATH leksah_datadir=`pwd` ./dist-newstyle/build/leksah-0.16.2.0/build/leksah/leksah $@
+PATH="$LEKSAH_SERVER_BUILD_DIR"'/leksah-server:'"$LEKSAH_SERVER_BUILD_DIR"'/leksahecho:'"$PATH" leksah_datadir=`pwd` ./"$LEKSAH_BUILD_DIR"/leksah/leksah $@
 
