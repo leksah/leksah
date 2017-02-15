98631268f61b42fa2e585e0524030774ffbc1011
Anton-Latukha <anton.latukha@gmail.com>
2017-02-15 16:50:07 +0200

    Modified ./leksah.sh, fixed server version

--- leksah.sh
+++ leksah.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-VERSION='0.16.2.2'
-LEKSAH_SERVER_BUILD_DIR=`pwd`'/dist-newstyle/build/leksah-server-'"$VERSION"'/build'
-LEKSAH_BUILD_DIR='dist-newstyle/build/leksah-'"$VERSION"'/build'
+LEKSAH_SERVER_VERSION='0.16.1.0'
+LEKSAH_VERSION='0.16.2.2'
+
+LEKSAH_SERVER_BUILD_DIR=`pwd`'/dist-newstyle/build/leksah-server-'"$LEKSAH_SERVER_VERSION"'/build'
+LEKSAH_BUILD_DIR='dist-newstyle/build/leksah-'"$LEKSAH_VERSION"'/build'
 
 cabal new-build -j4 exe:leksah-server exe:leksah exe:leksahecho || exit
-PATH="$LEKSAH_SERVER_BUILD_DIR"'/leksah-server:'"$LEKSAH_SERVER_BUILD_DIR"'/leksahecho:'"$PATH" leksah_datadir=`pwd` ./"$LEKSAH_BUILD_DIR"/leksah/leksah $@
-
+PATH="$LEKSAH_SERVER_BUILD_DIR"'/leksah-server:'"$LEKSAH_SERVER_BUILD_DIR"'/leksahecho:'"$PATH" leksah_datadir=`pwd` ./"$LEKSAH_BUILD_DIR"'/leksah/leksah' $@
