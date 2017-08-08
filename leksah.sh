ab65d3cce4ef85ac62ea436496659b852c659c4a
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-08-08 21:38:27 +1200

    Fix leksah.sh script

--- leksah.sh
+++ leksah.sh
@@ -5,17 +5,30 @@ LEKSAH_VERSION='0.16.2.2'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  OS="osx"
+else
+  OS="linux"
+fi
+
+ARCH="$(uname -m)"
+
+if [[ "$GHCVER" == "" ]]; then
+  GHCVER="$(ghc --numeric-version)"
+fi
+
 if [[ "$(cabal --numeric-version | cut -f 1 -d '.')" == "1" ]]; then
   echo "Using Cabal 1"
-  LEKSAH_SERVER_BUILD_DIR="$DIR"'/dist-newstyle/build/x86_64-osx/ghc-8.2.1/leksah-server-'"$LEKSAH_SERVER_VERSION"'/c/leksah-server/build'
-  LEKSAH_BUILD_DIR="$DIR"'/dist-newstyle/build/x86_64-osx/ghc-8.2.1/leksah-'"$LEKSAH_VERSION"'/c/leksah/build'
+  LEKSAH_SERVER_BUILD_DIR="$DIR/dist-newstyle/build/leksah-server-$LEKSAH_SERVER_VERSION/build"
+  LEKSAH_BUILD_DIR="$DIR/dist-newstyle/build/leksah-$LEKSAH_VERSION/build"
 elif [[ "$(cabal --numeric-version | cut -f 1 -d '.')" = "2" ]]; then
   echo "Using Cabal 2"
-  LEKSAH_SERVER_BUILD_DIR="$DIR"'/dist-newstyle/build/leksah-server-'"$LEKSAH_SERVER_VERSION"'/build'
-  LEKSAH_BUILD_DIR="$DIR"'/dist-newstyle/build/leksah-'"$LEKSAH_VERSION"'/build'
+  LEKSAH_SERVER_BUILD_DIR="$DIR/dist-newstyle/build/$ARCH-$OS/ghc-$GHCVER/leksah-server-$LEKSAH_SERVER_VERSION/c/leksah-server/build"
+  LEKSAH_BUILD_DIR="$DIR/dist-newstyle/build/$ARCH-$OS/ghc-$GHCVER/leksah-$LEKSAH_VERSION/c/leksah/build"
 else
   echo "Unknown cabal version $(cabal --numeric-version)"
 fi
 
+cabal new-configure --with-ghc="ghc-$GHCVER"
 cabal new-build -j4 exe:leksah-server exe:leksah exe:leksahecho || exit
-PATH="$LEKSAH_SERVER_BUILD_DIR"'/leksah-server:'"$LEKSAH_SERVER_BUILD_DIR"'/leksahecho:'"$PATH" leksah_datadir=`pwd` "$LEKSAH_BUILD_DIR"'/leksah/leksah' $@
+PATH="$LEKSAH_SERVER_BUILD_DIR/leksah-server:$LEKSAH_SERVER_BUILD_DIR/leksahecho:$PATH" leksah_datadir="$DIR" "$LEKSAH_BUILD_DIR/leksah/leksah" $@
