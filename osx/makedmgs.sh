acaf16e6507607d525c4f67c5c36d7f109b87d04
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-08-13 02:35:13 +1200

    Build Leksah binaries with GHC 8.2

--- osx/makedmgs.sh
+++ osx/makedmgs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -ex
 
-export GHCVERSION=-7.8.3
+export GHCVERSION=-8.0.2
 scripts/clean.sh
 osx/makedmg.sh
-export GHCVERSION=-7.10.2
-scripts/clean.sh
-osx/makedmg.sh
+#export GHCVERSION=-8.2.1
+#scripts/clean.sh
+#osx/makedmg.sh
 export GHCVERSION=
 scripts/clean.sh
 osx/makedmg.sh
