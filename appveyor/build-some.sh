a972905609189b901ad069a43f0c2a5fdc2894a3
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-08-15 17:24:51 +1200

    Try to fix AppVeyor

--- appveyor/build-some.sh
+++ appveyor/build-some.sh
@@ -4,13 +4,14 @@ if [ ! -d cache1 ]; then
   cabal update
   cabal install alex happy --disable-documentation
   cabal install haskell-gi --disable-documentation
-  cabal install gi-cairo gi-gdk gi-gdkpixbuf gi-gio gi-glib gi-gobject gi-gtk gi-gtksource gi-javascriptcore gi-pango gi-webkit gi-atk gi-soup -f-enable-overloading
+  cabal install gi-cairo gi-gdk gi-gdkpixbuf gi-gio gi-glib gi-gobject gi-gtk gi-gtksource gi-javascriptcore gi-pango gi-webkit gi-atk gi-soup \
+          -f-enable-overloading --disable-documentation --constraint='haskell-gi-overloading==0.*'
   mkdir cache1
   mv $APPDATA/ghc $APPDATA/cabal cache1
 elif [ ! -d cache2 ]; then
   mv cache1/* $APPDATA
   cabal update
-  cabal install gi-gtk-hs vendor/ltk ghcjs-dom --force-reinstalls
+  cabal install gi-gtk-hs vendor/ltk ghcjs-dom --disable-documentation --force-reinstalls --constraint='haskell-gi-overloading==0.*'
   mkdir cache2
   mv $APPDATA/ghc $APPDATA/cabal cache2
 elif [ ! -d cache3 ]; then
