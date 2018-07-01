fbfe3452f01df43ff163e01ce32e9c8d068c80c1
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-07-01 20:33:51 +1200

    Try using cabal new-build on AppVeyor

--- appveyor/build-some.sh
+++ appveyor/build-some.sh
@@ -3,22 +3,20 @@
 if [ ! -d cache1 ]; then
   cabal update
   cabal install cabal-install --disable-documentation
-  cabal install -j1 alex happy --disable-documentation
-  cabal install -j1 haskell-gi --disable-documentation
-  cabal install -j1 gi-cairo gi-gdk gi-gdkpixbuf gi-gio gi-glib gi-gobject gi-gtk gi-gtksource gi-javascriptcore gi-pango gi-webkit gi-atk gi-soup \
+  cabal new-build -gi-cairo gi-gdk gi-gdkpixbuf gi-gio gi-glib gi-gobject gi-gtk gi-gtksource gi-javascriptcore gi-pango gi-webkit gi-atk gi-soup \
           -f-enable-overloading --disable-documentation --constraint='haskell-gi-overloading==0.*'
   mkdir cache1
   mv $APPDATA/ghc $APPDATA/cabal cache1
 elif [ ! -d cache2 ]; then
   mv cache1/* $APPDATA
   cabal update
-  cabal install -j1 gi-gtk-hs vendor/ltk --disable-documentation --force-reinstalls --constraint='haskell-gi-overloading==0.*'
+  cabal new-build gi-gtk-hs vendor/ltk --disable-documentation --constraint='haskell-gi-overloading==0.*'
   mkdir cache2
   mv $APPDATA/ghc $APPDATA/cabal cache2
 elif [ ! -d cache3 ]; then
   mv cache2/* $APPDATA
   cabal update
-  cabal install -j1 ./ vendor/ltk vendor/leksah-server vendor/haskellVCSWrapper/vcswrapper vendor/haskellVCSGUI/vcsgui -fwebkit -f-yi -fpango -f-vty --constraint='haskell-gi-overloading==0.*' --only-dependencies
+  cabal new-build ./ vendor/ltk vendor/leksah-server vendor/haskellVCSWrapper/vcswrapper vendor/haskellVCSGUI/vcsgui -fwebkit -f-yi -fpango -f-vty --constraint='haskell-gi-overloading==0.*' --only-dependencies
   mkdir cache3
   mv $APPDATA/ghc $APPDATA/cabal cache3
 else
