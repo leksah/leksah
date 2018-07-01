bfa002e8ee1ab47eafa5bdaa969ba21cdfe6bf66
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-07-01 22:04:39 +1200

    Try using cabal new-build on AppVeyor

--- appveyor/build-some.sh
+++ appveyor/build-some.sh
@@ -6,19 +6,19 @@ if [ ! -d cache1 ]; then
   cabal new-build gi-cairo gi-gdk gi-gdkpixbuf gi-gio gi-glib gi-gobject gi-gtk gi-gtksource gi-pango gi-atk \
           -f-enable-overloading --disable-documentation --constraint='haskell-gi-overloading==0.*'
   mkdir cache1
-  mv $APPDATA/ghc $APPDATA/cabal cache1
+  mv $APPDATA/cabal cache1
 elif [ ! -d cache2 ]; then
   mv cache1/* $APPDATA
   cabal update
   cabal new-build gi-gtk-hs vendor/ltk --disable-documentation --constraint='haskell-gi-overloading==0.*'
   mkdir cache2
-  mv $APPDATA/ghc $APPDATA/cabal cache2
+  mv $APPDATA/cabal cache2
 elif [ ! -d cache3 ]; then
   mv cache2/* $APPDATA
   cabal update
   cabal new-build ./ vendor/ltk vendor/leksah-server vendor/haskellVCSWrapper/vcswrapper vendor/haskellVCSGUI/vcsgui -fwebkit -f-yi -fpango -f-vty --constraint='haskell-gi-overloading==0.*' --only-dependencies
   mkdir cache3
-  mv $APPDATA/ghc $APPDATA/cabal cache3
+  mv $APPDATA/cabal cache3
 else
   mv cache3/* $APPDATA
   cabal update
