763b7211a67860ee64521b1777540af46340fb57
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-07-08 05:01:24 +1200

    Pass args from leksah.bat to leksah.sh

--- leksah.bat
+++ leksah.bat
@@ -2,4 +2,4 @@ SET PATH=C:\tools\msys64\mingw64\bin;C:\tools\msys64\usr\bin;%PATH%
 SET PKG_CONFIG_PATH=C:\tools\msys64\mingw64\lib\pkgconfig
 SET XDG_DATA_DIRS=C:\tools\msys64\mingw64\share
 pacman -S --needed mingw64/mingw-w64-x86_64-pkg-config mingw64/mingw-w64-x86_64-gobject-introspection mingw64/mingw-w64-x86_64-gtksourceview3 mingw64/mingw-w64-x86_64-webkitgtk3
-bash leksah.sh
+bash leksah.sh %*
