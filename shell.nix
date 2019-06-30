e99affc9f67591c968c09d0a9e8c527a41e58c91
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2019-07-01 00:26:59 +1200

    Add plan only and shell only nix expressions

--- shell.nix
+++ shell.nix
@@ -1,17 +1,3 @@
-# To use this run `nix-shell`.  In the shell run `cabal new-build asterius`
-# to build the asterius executables.  Check that the resulting binaries
-# are in the PATH with `ahc-pkg list --global`.
 { haskellCompiler ? "ghc865" }:
-let
-  nixpkgs = import <nixpkgs> {};
-
-  nix-tools = (import ./. { inherit haskellCompiler; }).nix-tools;
-  hsPkgs = nix-tools._raw._config.hsPkgs;
-  cabalSystem = builtins.replaceStrings ["-darwin"] ["-osx"] nixpkgs.stdenv.system;
-in (hsPkgs.shellFor {
-    packages = ps: with ps; [ leksah ltk ];
-  }).overrideAttrs (oldAttrs: {
-    shellHook = (oldAttrs.shellHook or "") + ''
-      unset CABAL_CONFIG
-    '';
-  })
+(import ./. { inherit haskellCompiler; }).shells.ghc
+    
