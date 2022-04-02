343b2a3086707fd1c7a1ff31ec99507a549276f1
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2022-04-03 00:14:57 +1300

    Update gtkosxapplication

--- nix/overlays/gtk-debug.nix
+++ nix/overlays/gtk-debug.nix
@@ -9,4 +9,14 @@ self: super: {
         "--disable-glibtest"])
       (super.pkgs.lib.lists.flatten (oldAttrs.configureFlags or []));
   });
+  gtk-mac-integration = super.gtk-mac-integration.overrideAttrs (oldAttrs: {
+    version = "3.0.1";
+    src = self.fetchFromGitLab {
+      domain = "gitlab.gnome.org";
+      owner = "GNOME";
+      repo = "gtk-mac-integration";
+      rev = "gtk-mac-integration-3.0.1";
+      sha256 = "sha256-6k0pikco+6/XIB4A5Ej0qffopjivnUZiga6XjO6ogGk=";
+    };
+  });
 }
